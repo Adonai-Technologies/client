213ab3a1fdc96db8903b88924d3916bc564fed3a
Akua Konadu <akuakkonadu8@gmail.com>
2024-03-20 10:36:21 +0100

    new updates

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className='p-5 bg-primary flex justify-between'>
-      <h1 className="text-secondary text-4xl font-semibold">ADO-Tech</h1>
+      <h1 className="text-secondary text-4xl font-semibold"><a href=""><img className='w-20' src="images/Ado-tech.png" alt="" /></a></h1>
       <h1 className="text-white text-4xl font-semibold">A</h1>
       <h1 className="text-tertiary text-4xl font-semibold">K</h1>
     </div>
