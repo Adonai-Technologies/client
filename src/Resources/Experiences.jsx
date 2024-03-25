d7523f61c7e975dadbf8f9e2bbc71df4e06100eb
Akua Konadu <akuakkonadu8@gmail.com>
2024-03-25 10:59:51 +0100

    connected to mongoDB

--- src/Resources/Experiences.jsx
+++ src/Resources/Experiences.jsx
@@ -1,24 +1,20 @@
 export const ExperiencesData = [
-    {
-        _id: "1234567890",
-        company: "Adonai Technologies",
-        period: "2021-present",
-        title: "Full Stack Developer",
-        description: ""
-    },
-    {
-        _id: "1099887765432",
-        company: "100devs Academy",
-        period: "2022-present",
-        description: "",
-        title: "Mern Stack Developer",
-    },
-    {
-        _id: "1234567890",
-        company: "Self Employed",
-        period: "2020-present",
-        description: "2020-present",
-        title: "Freelancer",
-    },
-
-]
+	{
+		company: "Adonai Technologies",
+		period: "2021-present",
+		title: "Full Stack Developer",
+		description: "",
+	},
+	{
+		company: "100devs Academy",
+		period: "2022-present",
+		description: "",
+		title: "Mern Stack Developer",
+	},
+	{
+		company: "Self Employed",
+		period: "2020-present",
+		description: "2020-present",
+		title: "Freelancer",
+	},
+];
